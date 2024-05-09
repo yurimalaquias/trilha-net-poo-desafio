712c09cd8b196d1ef42d90b69ef8a7b830daa99b
Yuri Malaquias <yurimalaquias.fb@gmail.com>
2024-05-09 17:09:02 -0300

    Solucao do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+// Nokia
+Smartphone nokia = new Nokia(numero: "16999999999", modelo: "Tijolão", imei: "asdqwezxc", memoria: 12);
+nokia.Ligar();
+nokia.InstalarAplicativo("Roblox");
+
+Console.WriteLine("-x-x-x-x-x-x-x-x-x-");
+
+Smartphone iphone = new Iphone(numero: "1688888888", modelo: "15 PRO MAX TURBO", imei: "poijklmnb", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Notion");
